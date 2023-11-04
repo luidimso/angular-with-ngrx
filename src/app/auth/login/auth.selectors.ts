afac2b2e4e983152ecd7713228b32a56ebf1ea1e
luidimso <luidi_mso@outlook.com>
2023-11-04 06:03:25 -0300

    Implementing feature selector

--- src/app/auth/login/auth.selectors.ts
+++ src/app/auth/login/auth.selectors.ts
@@ -1,7 +1,11 @@
-import { createSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { AuthState } from "../reducers";
+
+// a featureSelector queries a property and gives a type for it
+export const selectAuthState = createFeatureSelector<AuthState>("auth");
 
 //the createSelector method avoid to run its implementation again if the input (state[zuth]) has the same value of the last run
 //the input value is stored in cache memory
-export const isLoggedIn = createSelector(state => state["auth"], (auth) => !!auth.user);
+export const isLoggedIn = createSelector(selectAuthState, (auth) => !!auth.user);
 
 export const isLoggedOut = createSelector(isLoggedIn, loggedIn => !loggedIn);
