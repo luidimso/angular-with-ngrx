a3dfcae1556639902a7d398aac1e5efa5a3d76e3
luidimso <luidi_mso@outlook.com>
2023-11-04 20:47:04 -0300

    Improving login side effect

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,20 +1,21 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { AuthActions } from "./actions-types";
+import { tap } from "rxjs/operators";
 
 //Effects is to do side effects after a action is dispatched
 //For exemple: save on local storage the login authentication after the login action be dispatched
 //The effect may only be declared on the module (EffectsModule.forFeature)
 @Injectable()
 export class AuthEffects {
+    login$ = createEffect(() => {
+        return this.action$.pipe(ofType(AuthActions.login), tap(action => {
+            console.log("Login Side Effect");
+            localStorage.setItem("user", JSON.stringify(action.user));
+        }));
+    }, {dispatch: false});
+
     constructor(
         private action$: Actions
-    ) {
-        action$.subscribe(action => {
-            switch(action.type) {
-                case "[Login Page] User Login":
-                    console.log("Login Side Effect");
-                    localStorage.setItem("user", JSON.stringify(action["user"]));
-            }
-        });
-    }
+    ) {}
 }
