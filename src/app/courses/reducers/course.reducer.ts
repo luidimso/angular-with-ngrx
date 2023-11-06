b650f25c1da2b13333103c7a1235607cbce81ba5
luidimso <luidi_mso@outlook.com>
2023-11-06 20:55:21 -0300

    Implementing reducer to update action

--- src/app/courses/reducers/course.reducer.ts
+++ src/app/courses/reducers/course.reducer.ts
@@ -17,11 +17,16 @@ export const initialCoursesState = adapter.getInitialState({
     allCoursesLoaded: false
 });
 
+// on createReducer we can pass through the method "on" the effect espected on a action, in that example we have 2 actions with effects: allCoursesLoaded and courseUpdate
+// the adapter, which is a entity adapter, have the CRUD methods to the store, like addMany, update
+// the reducers only apply the actions on the store datas
 export const coursesReducer = createReducer(initialCoursesState, on(CourseActions.allCoursesLoaded, (state, action) => {
     return adapter.addMany(action.courses, {
         ...state,
         allCoursesLoaded: true
     });
+}), on(CourseActions.courseUpdate, (state, action) => {
+    return adapter.updateOne(action.update, state);
 }));
 
 export const {selectAll} = adapter.getSelectors();
