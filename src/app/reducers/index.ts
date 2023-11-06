76d5378e19cb389a22ac44541aa9f71212974325
luidimso <luidi_mso@outlook.com>
2023-11-06 04:37:37 -0300

    Implementing metareducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -16,5 +16,13 @@ export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
 
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log(state);
+    console.log(action);
 
-export const metaReducers: MetaReducer<AppState>[] = isDevMode() ? [] : [];
+    return reducer(state, action);
+  }
+}
+
+export const metaReducers: MetaReducer<AppState>[] = isDevMode() ? [logger] : [];
