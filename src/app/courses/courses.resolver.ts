fdccd1abbbb32ce7aabd49353ef38cc8e30e1956
luidimso <luidi_mso@outlook.com>
2023-11-06 11:05:43 -0300

    Implementing courses resolver

---
+++ src/app/courses/courses.resolver.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { AppState } from "../reducers";
+import { finalize, first, tap } from "rxjs/operators";
+import { loadAllCourses } from "./courses.actions";
+
+@Injectable()
+export class CoursesResolver implements Resolve<any> {
+    loading = false;
+
+    constructor(
+        private store: Store<AppState>
+    ) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.store.pipe(tap(() => {
+            if(!this.loading) {
+                this.loading = true;
+                this.store.dispatch(loadAllCourses());
+            }
+        }), first(), finalize(() => this.loading = false));
+    }
+}
