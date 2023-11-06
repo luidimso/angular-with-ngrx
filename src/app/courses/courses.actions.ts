01688fa298e3e8c970eef871db32d4999566c178
luidimso <luidi_mso@outlook.com>
2023-11-06 10:45:26 -0300

    Implementing courses actions

---
+++ src/app/courses/courses.actions.ts
@@ -0,0 +1,5 @@
+import { createAction } from "@ngrx/store";
+
+export const loadAllCourses = createAction("[Courses Roesolver] Load All Courses");
+
+export const allCoursesLoaded = createAction("[Load Courses Effect] All Courses Loaded");
