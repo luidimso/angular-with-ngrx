0f5c6442f1d4a2b998eaf523ed225cb0a5623454
luidimso <luidi_mso@outlook.com>
2023-11-06 20:43:49 -0300

    Implementing update action

--- src/app/courses/courses.actions.ts
+++ src/app/courses/courses.actions.ts
@@ -1,8 +1,11 @@
 import { createAction, props } from "@ngrx/store";
 import { Course } from "./model/course";
+import { Update } from "@ngrx/entity";
 
 // loadAllCourses action only is called when enter on courses home page, then it run the action allCoursesLoaded by effect 
 
 export const loadAllCourses = createAction("[Courses Resolver] Load All Courses");
 
 export const allCoursesLoaded = createAction("[Load Courses Effect] All Courses Loaded", props<{courses: Course[]}>());
+
+export const courseUpdate = createAction("[Edit Course Dialog] Course Updated", props<{update: Update<Course>}>());
--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -4,6 +4,10 @@ import {Course} from '../model/course';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {CoursesHttpService} from '../services/courses-http.service';
+import { AppState } from '../../reducers';
+import { Store } from '@ngrx/store';
+import { Update } from '@ngrx/entity';
+import { CourseActions } from '../actions-type';
 
 @Component({
   selector: 'course-dialog',
@@ -26,7 +30,7 @@ export class EditCourseDialogComponent {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<EditCourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data,
-    private coursesService: CoursesHttpService) {
+    private store: Store<AppState>) {
 
     this.dialogTitle = data.dialogTitle;
     this.course = data.course;
@@ -63,12 +67,18 @@ export class EditCourseDialogComponent {
       ...this.form.value
     };
 
-    this.coursesService.saveCourse(course.id, course)
-      .subscribe(
-        () => this.dialogRef.close()
-      )
 
+    // The Update interface receives 2 paramters
+    // id: the id of the data stored on store
+    // changes: the new object with the values chnaged, in that example course is the object with the changes with the form values
+    const update: Update<Course> = {
+      id: course.id,
+      changes: course      
+    };
 
+    this.store.dispatch(CourseActions.courseUpdate({update}));
+
+    this.dialogRef.close();
   }
 
 
