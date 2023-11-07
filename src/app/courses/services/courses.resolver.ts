1a7b6a73328f25fac8d0650679a5de87c2ad280b
luidimso <luidi_mso@outlook.com>
2023-11-07 11:42:33 -0300

    Adding the logic to see if data was already loaded on store by the resolver

--- src/app/courses/services/courses.resolver.ts
+++ src/app/courses/services/courses.resolver.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 import { CourseEntityService } from "./courses-entity.service";
-import { map } from "rxjs/operators";
+import { filter, first, map, tap } from "rxjs/operators";
 
 @Injectable()
 export class CoursesResolver implements Resolve<boolean> {
@@ -12,7 +12,13 @@ export class CoursesResolver implements Resolve<boolean> {
 
     //the CourseEntityService can reach directly the backend service database, and have the CRUD methods such as getAll, so getAll will return all the courses from database as entity
     //getAll method call to the backend service and already dispatch it to the store
+    //resolve needs to pass boolean because it controls the route guard, which defines if the page can accessed
+    //CourseEntityService has the property loaded that returns a boolean that indicats if the data already was loaded on store
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        return this.coursesService.getAll().pipe(map(courses => !!courses));
+        return this.coursesService.loaded$.pipe(tap(loaded => {
+            if(!loaded) {
+                this.coursesService.getAll();
+            }
+        }), filter(loaded => !!loaded), first());
     }
 }
