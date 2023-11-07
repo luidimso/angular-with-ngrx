0fd929ff72dfcae5fe09531c748e88da9e1fefcc
luidimso <luidi_mso@outlook.com>
2023-11-06 21:06:47 -0300

    Implementing update effect to update the data on backend database

--- src/app/courses/courses.effect.ts
+++ src/app/courses/courses.effect.ts
@@ -7,12 +7,19 @@ import { CoursesHttpService } from "./services/courses-http.service";
 @Injectable()
 export class CoursesEffects {
     // This effect first call the service findALLCourses, then it is finished, it calls the action allCoursesLoaded passing the courses returned as paramter on first call
-
+    // ConcatMao was used to run each method on order, like first we have to call coursesService.findAllCourses, then call the allCoursesLoaded action passing corses as paramter
     loadCourses$ = createEffect(() => this.action$.pipe(ofType(CourseActions.loadAllCourses), concatMap(action => {
         return this.coursesService.findAllCourses();
     }), map(courses => {
         return CourseActions.allCoursesLoaded({courses})
     })));
+
+    // dispatch false mean that this effect will not dispatch any action after
+    saveCourse$ = createEffect(() => this.action$.pipe(ofType(CourseActions.courseUpdate), concatMap(action => {
+        return this.coursesService.saveCourse(action.update.id, action.update.changes);
+    })), {
+        dispatch: false
+    });
 
     constructor(
         private action$: Actions,
